bec41fc7e5852154758f6f66b6602475ad9d2574
Evan Cook <evancook711@gmail.com>
2026-01-14 15:39:56 -0500

    Fixed zip script to get proper zip file structure, and fixed main_extension.gd to properly load the pinned stats container

--- extensions/main_extension.gd
+++ extensions/main_extension.gd
@@ -3,6 +3,6 @@ extends "res://main.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
-	var pinned_stats_container = PinnedStatsContainer.new()
+	var pinned_stats_container = preload("res://mods-unpacked/Revy-PinStats/pinned_stats_container.tscn").instance()
 	var life_container_p1 = $UI / HUD / LifeContainerP1
 	life_container_p1.add_child(pinned_stats_container)
--- make_steam_workshop_zip.sh
+++ make_steam_workshop_zip.sh
@@ -13,7 +13,10 @@ rm mod-temp/mods-unpacked/Revy-PinStats/make_steam_workshop_zip.sh
 mkdir mod-temp/.import
 cp -r .import/PinnedStats_* mod-temp/.import
 
-zip -qr "PinnedStats.zip" mod-temp
+cd mod-temp
+zip -qr "../PinnedStats.zip" .
+
+cd ..
 
 rm -rf mod-temp
 
