02393aa69c571b441c78c65b0fd9aeb0eb7219a8
Evan Cook <evancook711@gmail.com>
2026-01-14 13:10:29 -0500

    Added script to build zip file for Steam Workshop

---
+++ make_steam_workshop_zip.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+cd ../..
+
+mkdir mod-temp
+
+cp -r mods-unpacked mod-temp
+
+rm -rf mod-temp/mods-unpacked/Revy-PinStats/.git
+rm mod-temp/mods-unpacked/Revy-PinStats/make_steam_workshop_zip.sh
+
+mkdir mod-temp/.import
+cp -r .import/PinnedStats_* mod-temp/.import
+
+zip -r "PinnedStats.zip" mod_temp
+
+rm -rf mod-temp
+
+
